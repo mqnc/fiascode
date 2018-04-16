aaeee66ae5f8b7ed6424cd149dd9a1ad83f9c30b
unknown <kunze@gestalt-robotics.com>
2018-04-16 18:17:59 +0200

    added stray exceptions, groups and identifiers

--- parsleytest/pt.py
+++ parsleytest/pt.py
@@ -4,43 +4,71 @@ cd = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(cd + "/parsley")
 import parsley # rename parsley/parsley.py to parsley/__init__.py
 
-'''
-grammar = parsley.makeGrammar("""
-test (~('a' | 'b') anything)*:t anything*-> ''.join(t).upper()
-""", {})
+if True:
+	grammar = parsley.makeGrammar("""
+	
+	word = <letter+>
+	term = '[' (~']' (word:w | anything))*:i ']' -> (''.join(i), w)
+	
+	test term:t -> 'Term is "' + t[0] + '", last word is "' + t[1] + '"'
+	
+	
+	""", {})
 
-print(grammar("sfdsabwet").test())
+	print(grammar("[ich +bin ein 20-Test]").test())
 
-'''
 
+def exception(txt):
+	raise SyntaxError(txt)
+	return txt
+	
 fiascode = parsley.makeGrammar("""
 
 code = (known | anything)*:c -> ''.join(c)
 
-known = (comment | string | substitution)
 
-comment = (comment1 | comment2):c -> 'COMMENT[' + c + ']'
+known = (comment | string | substitution | group | stray)
+stray = (groupstray | ifstray):estray -> exception('Stray ' + estray + ' detected')
+
+comment = (comment1 | comment2)
 comment1 = <'/*' (~'*/' anything)* '*/'>
 comment2 = <'//' (~'\n' anything)* '\n'>
 
-string = <'"' (escaped | ~'"' anything)* '"'>:s -> 'STRING[' + s + ']'
+string = <'"' (escaped | ~'"' anything)* '"'>
 escaped = <'\\\\' anything> # we need a double backslash escape here for representing one backslash
 
-substitution = ifstmt
+group = (parenthesed | bracketed | braced)
+parenthesed = '(' (~')' (known | anything))*:body ')' -> '(' + ''.join(body) + ')'
+bracketed   = '[' (~']' (known | anything))*:body ']' -> '[' + ''.join(body) + ']'
+braced      = '{' (~'}' (known | anything))*:body '}' -> '{' + ''.join(body) + '}'
+groupstray = (')' | ']' | '}')
+
+identifier = <(letter | '_') (letter | '_' | digit)*>
+
+
+substitution = ifstmt | forstmt # actual fiascode 
+
 
 ifstmt     = 'If'     condition:cond 'Then'   ifbody:body (elseifstmt | elsestmt | endifstmt):tail ->      'if(' + cond + '){' + ''.join(body) + '}' + tail
 elseifstmt = 'Elseif' condition:cond 'Then'   ifbody:body (elseifstmt | elsestmt | endifstmt):tail -> 'else if(' + cond + '){' + ''.join(body) + '}' + tail
 elsestmt   = 'Else'                         elsebody:body                          endifstmt :tail -> 'else{'                  + ''.join(body) + '}' + tail
 endifstmt  = 'Endif' -> ''
 
-condition = (known | ~'Then' anything)*:cond -> ''.join(cond)
-ifbody    = (known | ~('Elseif' | 'Else' | 'Endif') anything)*:body -> ''.join(body)
-elsebody  = (known | ~('Endif') anything)*:body -> ''.join(body)
+condition = (~'Then' (known | anything))*:cond -> ''.join(cond)
+ifbody    = (~('Elseif' | 'Else' | 'Endif') (known | anything))*:body -> ''.join(body)
+elsebody  = (~'Endif' (known | anything))*:body -> ''.join(body)
+
+ifstray = ('Elseif' | 'Else' | 'Endif' | 'Then') # those should not be encountered first
 
 
-#forstmt = 'For' ws block:b1 ws 'Do' ws block:b2 ws 'Loop' -> 'for(' + b1 + '){' + b2 + '}'
 
-""", {})
+forstmt = 'For' 
+
+""", {'exception':exception})
+
+    #for(int x=0, y=0, w=3, h=3; y<h; x++, y+=x>=w?1:0, x=x>=w?0:x){
+   #     std::cout << x << y << "\n";
+  #  }
 
 print(fiascode("""
 
@@ -54,8 +82,6 @@ If a>b Then
 		k=4
 	Else
 		a=7
-	Elseif h=8 Then
-		g=3
 	Endif
 Endif
 
